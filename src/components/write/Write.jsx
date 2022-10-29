2ee36181c7b7d9f06757fab3a9fda48a0043811a
kki147 <rla123dlf@gmail.com>
2022-10-29 10:42:21 +0900

    add : writeSlice

--- src/components/write/Write.jsx
+++ src/components/write/Write.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Write() {
-  return (
-    <div>Write</div>
-  )
+	return (
+		<form>
+			<label>제목</label>
+			<input type="text" name="title" style={{ border: "1px solid" }} />
+			<br />
+
+			<label>내용</label>
+			<input type="text" name="coment" style={{ border: "1px solid" }} />
+
+			<br />
+
+			<button type="submit" name="button">
+				버튼
+			</button>
+		</form>
+	);
 }
 
-export default Write
+export default Write;
