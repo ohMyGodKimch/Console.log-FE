e750e63bd9008f95b69a4fa9b6024c55e69cdc96
kki147 <rla123dlf@gmail.com>
2022-10-29 17:12:32 +0900

    add : get

---
+++ src/pages/mainlist/MainlistPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainList } from "../../components/mainlist";
+
+function MainlistPage() {
+	return (
+		<>
+			<MainList />
+		</>
+	);
+}
+
+export default MainlistPage;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignInPage, SignUpPage } from "../pages/join";
 import JoinLayout from "../layout/join";
-
 import WritePage from "../pages/write/WritePage";
+import { MainlistPage } from "../pages/mainlist";
 
 const Router = () => {
 	return (
@@ -13,6 +13,7 @@ const Router = () => {
 						<Route path="signup" element={<SignUpPage />} />
 						<Route path="signin" element={<SignInPage />} />
 					</Route>
+					<Route path="/main" element={<MainlistPage />}></Route>
 					<Route path="/boadrs" element={<WritePage />}></Route>
 				</Routes>
 			</BrowserRouter>
